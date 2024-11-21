bb88c24263fdf1f5469b5895b2901e99b53f45dc
nshush <kagekao137@gmail.com>
2024-11-21 21:42:28 +0300

    Added Task1

---
+++ Workshop/Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+    public class MyCustomException : Exception
+    {
+        public MyCustomException(string message) : base(message) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[]
+            {
+                new DivideByZeroException("Ошибка: Деление на ноль."),
+                new ArgumentNullException("Ошибка: Аргумент не может быть null."),
+                new IndexOutOfRangeException("Ошибка: Индекс был вне диапазона."),
+                new InvalidOperationException("Ошибка: Недопустимая операция."),
+                new MyCustomException("Ошибка: Это мое собственное исключение.")
+            };
+
+            foreach (var exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (MyCustomException ex)
+                {
+                    Console.WriteLine($"Обработано собственное исключение: {ex.Message}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Обработано исключение деления на ноль: {ex.Message}");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine($"Обработано исключение: {ex.Message}");
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Обработано исключение выхода за границы: {ex.Message}");
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine($"Обработано исключение недопустимой операции: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally выполнен.\n");
+                }
+            }
+        }
+    }
